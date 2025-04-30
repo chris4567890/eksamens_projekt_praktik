f39a9a20750d6bf38b2a6b8f85b5053b40923418
chris4567890 <starchristian4@hotmail.com>
2025-04-30 11:03:12 +0200

    made shell for BarselsService

---
+++ eksamn_project/eksamn_project.Server/Services/BarselsService.cs
@@ -0,0 +1,30 @@
+﻿namespace eksamn_project.Server.Services
+{
+    public class BarselsService
+    {
+        public int CalculateAmountOfBarsel(BarselsModel model) 
+        {
+            int amountOfWeeks = 0;
+            if (model.Gender == "female")
+            {
+                if (model.Arbejdsstatus == "private")
+                {
+                    if (model.OverEnskomst == true)
+                    {
+                        amountOfWeeks = 36;
+                    }
+                    else 
+                    {
+                        amountOfWeeks = 14;
+                    }
+                }
+                else if (model.arbejdsstatus == "offentlig")
+                {
+                    amountOfWeeks = 36;
+                }
+                
+            }
+            return amountOfWeeks; 
+        }
+    }
+}
