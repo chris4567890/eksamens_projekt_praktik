b447c4f8cc5f1586d92481028bf8686a9e6a525b
chris4567890 <starchristian4@hotmail.com>
2025-05-05 10:12:02 +0200

    made shells for calculator and expanded frontend so all options are available

--- eksamn_project/eksamn_project.Server/Helpers/BarselsHelper.cs
+++ eksamn_project/eksamn_project.Server/Helpers/BarselsHelper.cs
@@ -9,14 +9,21 @@ namespace eksamn_project.Server.Helpers
         {
             SingleMomCalculator singleMomCalculator = new SingleMomCalculator();
             SingleFatherCalculator singleFatherCalculator = new SingleFatherCalculator();
+            MotherFatherCalculator motherFatherCalculator = new MotherFatherCalculator();
+            MomMomCalculator momMomCalculator = new MomMomCalculator();
+            FatherFatherCalculator fatherFatherCalculator = new FatherFatherCalculator();
             switch (Relationship)
             {
                 case "single_mother":
                     return singleMomCalculator.calculate(Relationship,Overnskomst,Arbejdsstatus,Multiplechildren,SingleChild);    
-                        
                 case "single_father":
                     return singleFatherCalculator.calculate(Relationship, Overnskomst, Arbejdsstatus, Multiplechildren, SingleChild);
-                    
+                case "mother_father":
+                    return motherFatherCalculator.calculate(Relationship,Overnskomst,Arbejdsstatus,Multiplechildren,SingleChild);
+                case "mother_mother":
+                    return momMomCalculator.calculate(Relationship,Overnskomst,Arbejdsstatus,Multiplechildren,SingleChild);
+                case "father_father":
+                    return fatherFatherCalculator.calculate(Relationship,Overnskomst,Arbejdsstatus,Multiplechildren,SingleChild);
                 default:
                     return 0;
             }
--- eksamn_project/eksamn_project.client/src/components/Forholds_Valg.jsx
+++ eksamn_project/eksamn_project.client/src/components/Forholds_Valg.jsx
@@ -15,6 +15,10 @@ function Forholds_valg({ formData, setFormData }) {
                 <label htmlFor="single_father">enlig mand</label>
                 <input type="radio" name="relationship" id="mother_father" value="mother_father" onChange={handleChange} required/>
                 <label htmlFor="mother_father">gift kvinde og mand</label>
+                <input type="radio" name="relationship" id="mother_mother" value="mother_mother" onChange={handleChange} required />
+                <label htmlFor="mother_mother">mor og mor</label>
+                <input type="radio" name="relationship" id="father_father" value="father_father" onChange={handleChange} required />
+                <label htmlFor="father_father">far og far</label>
                 
             </form>
         </div>
